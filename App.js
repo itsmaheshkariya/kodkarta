0a9c331504cb8abc94786281729e112ee2f2627c
mahesh-kariya <mahesh.kariya@revealgc.com>
2024-06-19 15:30:40 +0530

    New Theme Updated

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Pacifico_400Regular } from '@expo-google-fonts/pacifico';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hey Aditya</Text>
-      <Text>Hey Mahesh</Text>
-      <Text>Hey KodKarta</Text>
+      <Text style={styles.text}>KodKarta.com</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +23,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF9800', // Material Orange color
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+    fontFamily: 'Pacifico_400Regular',
   },
 });
